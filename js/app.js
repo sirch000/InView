e407655aaa27cbb7ecbd2b05cae1ca94e17ce197
teachme10code <chris.ward@servicenow.com>
2015-06-21 20:06:01 -0700

    dynamic floorplan image loading

--- js/app.js
+++ js/app.js
@@ -3,7 +3,9 @@
 angular.module('inView', [])
   .controller('inViewController', ['$scope', '$http', function($scope, $http) {
 	$http.get('data.json').success(function(data) {
+		$scope.floorplans = data.flooring.floorplans;
+		$scope.fp1 = $scope.floorplans[0].url;
 		$scope.flooring = data.flooring;
-		console.log($scope.flooring);
+		//console.log($scope.floorplans);
   	});
   }]);
