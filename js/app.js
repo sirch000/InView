d29572c627278b1ee9fc2b4e37d6859c73cbf0a6
teachme10code <chris.ward@servicenow.com>
2015-08-02 22:47:11 -0700

    slider and supporting libraries

--- js/app.js
+++ js/app.js
@@ -1,11 +1,83 @@
 "use strict";
 
-angular.module('inView', [])
-  .controller('inViewController', ['$scope', '$http', function($scope, $http) {
-	$http.get('data.json').success(function(data) {
-		$scope.floorplans = data.flooring.floorplans;
-		$scope.fp1 = $scope.floorplans[0].url;
-		$scope.flooring = data.flooring;
-		//console.log($scope.floorplans);
-  	});
-  }]);
+angular.module('website', ['ngAnimate'])
+    .controller('MainCtrl', function ($scope, $http) {
+    	$http.get('data.json').success(function(data) {
+			$scope.floorplans = data.flooring.floorplans; //floorplan images
+			$scope.flooring = data.flooring; //colored swatches
+			$scope.floorplanList = [];
+		})
+
+		angular.forEach($scope.floorplans, function(floorplan, key) {
+			this.push(floorplan.url);
+		}, $scope.floorplanList);
+
+
+    	$scope.slides = [
+            {image: 'img/fp1.png'},
+            {image: 'img/fp2.png'},
+            {image: 'img/fp1.png'}
+        ];
+
+        $scope.direction = 'left';
+        $scope.currentIndex = 0;
+
+        $scope.setCurrentSlideIndex = function (index) {
+            $scope.direction = (index > $scope.currentIndex) ? 'left' : 'right';
+            $scope.currentIndex = index;
+        };
+
+        $scope.isCurrentSlideIndex = function (index) {
+            return $scope.currentIndex === index;
+        };
+
+        $scope.prevSlide = function () {
+            $scope.direction = 'left';
+            $scope.currentIndex = ($scope.currentIndex < $scope.slides.length - 1) ? ++$scope.currentIndex : 0;
+        };
+
+        $scope.nextSlide = function () {
+            $scope.direction = 'right';
+            $scope.currentIndex = ($scope.currentIndex > 0) ? --$scope.currentIndex : $scope.slides.length - 1;
+        };
+    })
+
+    .animation('.slide-animation', function () {
+        return {
+            beforeAddClass: function (element, className, done) {
+                var scope = element.scope();
+
+                if (className == 'ng-hide') {
+                    var finishPoint = element.parent().width();
+                    if(scope.direction !== 'right') {
+                        finishPoint = -finishPoint;
+                    }
+                    TweenMax.to(element, 0.5, {left: finishPoint, onComplete: done });
+                }
+                else {
+                    done();
+                }
+            },
+            removeClass: function (element, className, done) {
+                var scope = element.scope();
+
+                if (className == 'ng-hide') {
+                    element.removeClass('ng-hide');
+
+                    var startPoint = element.parent().width();
+                    if(scope.direction === 'right') {
+                        startPoint = -startPoint;
+                    }
+
+                    TweenMax.fromTo(element, 0.5, { left: startPoint }, {left: 0, onComplete: done });
+                }
+                else {
+                    done();
+                }
+            }
+        };
+    });
+
+
+
+
